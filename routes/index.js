5f3dd8ac58bdc20ce985fee8b3a79c1b1cfc677f
Sebas <seplarui@gmail.com>
2018-01-21 12:30:25 +0100

    Acceso a BBDD Mysql

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var mysql= require('mysql');
+
+var connection= mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'cine'
+});
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Peliculas' });
+  connection.connect();
+  connection.query('select * from peliculas', function(err,rows, fileds){
+    if(err) throw err;
+    console.log('El resultado es: ', rows[0]);
+  });
+ 
 });
+
 
 module.exports = router;
