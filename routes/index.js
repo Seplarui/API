27800f710dbc22a73f2a4327388aa1524e224e4b
Sebas <seplarui@gmail.com>
2018-01-22 08:23:48 +0100

    Cambios

--- routes/index.js
+++ routes/index.js
@@ -11,11 +11,12 @@ var connection= mysql.createConnection({
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Peliculas' });
-  connection.connect();
-  connection.query('select * from peliculas', function(err,rows, fileds){
+  //res.render('index', { title: 'Peliculas' });
+  
+  connection.query('select * from peliculas', function(err,rows, fields){
     if(err) throw err;
-    console.log('El resultado es: ', rows[0]);
+    console.log('El resultado es: ', rows[0].Titulo);
+    res.json(rows[0]);
   });
  
 });
